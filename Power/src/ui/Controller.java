bed5fbbfc632ad13b523348f763ab964df0273b2
Bela Balint <bbeluska@yahoo.com>
2017-04-24 13:41:29 +0300

    Layouting
    Change-Id: I56a9b9e59b4eaf674791cc6a55a85769156148a5

--- Power/src/ui/Controller.java
+++ Power/src/ui/Controller.java
@@ -1,10 +1,61 @@
 package ui;
 
 public class Controller {
-	View mainView;
+	private View view;
+	private String[] comPorts;
+	private String selectedComPort;
 	
 	public Controller(){
-		mainView = new View();
+		comPorts = findAvailableComPorts();
+		view = new View(this);
+		
 	}
+	
+	/**
+	 * connect to the selected com port
+	 */
+	public void connect(){
+		view.setConnectButtonEnabled(false);
+		//try to connect
+//		if (connected){
+//			view.setConnectButtonConnect(false);
+//		} else {
+//			view.setConnectButtonConnect(true);
+//		}
+		view.setConnectButtonEnabled(true);
+	}
+	
+	/**
+	 * disconnect from com port
+	 */
+	public void disconnect(){
+		view.setConnectButtonConnect(true);
+	}
+	
+	private String[] findAvailableComPorts(){
+		//TODO: To be implemented
+		return new String[] {"COM1", "COM2", "","COM3"};
+	}
+	
+	//getters and setters
+	public String[] getAvailableComPorts(){
+		return this.comPorts;
+	}
+
+	public String getSelectedComPort() {
+		return selectedComPort;
+	}
+
+	public void setSelectedComPort(String selectedComPort) {
+		if (!selectedComPort.equals(null) && !"".equals(selectedComPort)){
+			this.selectedComPort = selectedComPort;
+			view.setConnectButtonEnabled(true);
+			System.out.println(selectedComPort + " is selected!");
+		} else {
+			view.setConnectButtonEnabled(false);
+		}
+		
+	}
+	
 
 }
